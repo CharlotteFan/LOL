6428757ca175ca0b47b52bbe1e2a61fdb4bffb7d
CharlotteFan <15269102172@163.com>
2020-09-16 18:50:56 +0800

    testCommit

---
+++ LOLservice/Controllers/tempController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LOLservice.Controllers
+{
+    [ApiController]
+    [Route("LOL")]
+    public class tempController : ControllerBase
+    {
+        public tempController()
+        {
+        }
+
+        [HttpGet, Route("hello/{name}")]
+        public string SayHello(string name)
+        {
+            return $"Hello, {name}";
+        }
+    }
+}
